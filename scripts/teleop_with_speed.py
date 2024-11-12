f82f34faafcb2d8437013176ccce3c2bbc652714
Aiswarya Shajan <AS4213@live.mdx.ac.uk>
2024-11-12 03:48:38 -0800

    -Fixed the esc issue.
    -Reason: The listener was placed inside the while loop. This caused multiple listener threads in the program. So even if the listener was stopped (at return false) it would just restart within the loop.
    Also lead to few errors with display used ( Xlib11).
    
    -Restructured the code: Earlier, with the listener inside the loop, the pub.publish message was mixed with the listener on_press/release functions. Separating the listener and while loop, makes it simpler.
    The pub.publish is only written once inside the while loop.

--- scripts/teleop_with_speed.py
+++ scripts/teleop_with_speed.py
@@ -10,56 +10,49 @@ def teleoperation():
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     rate = rospy.Rate(10)
     twist = Twist()
-    while not rospy.is_shutdown():
-        def on_press(key):
-            try:
-                if key == Key.up:
-                    twist.linear.x = 1
-                    pub.publish(twist)
-                elif key == Key.down:
-                    twist.linear.x = -1
-                    pub.publish(twist)
-                elif key == Key.left:
-                    twist.angular.z = 1
-                    pub.publish(twist)
-                elif key == Key.right:
-                    twist.angular.z = -1
-                    pub.publish(twist)
-                else:
-                    pass
-            except AttributeError:
-                pass
-        
-        def on_release(key):
-            try:
-                if key == Key.up:
-                    twist.linear.x = 0
-                    pub.publish(twist)
-                elif key == Key.down:
-                    twist.linear.x = 0
-                    pub.publish(twist)
-                elif key == Key.left:
-                    twist.angular.z = 0
-                    pub.publish(twist)
-                elif key == Key.right:
-                    twist.angular.z = 0
-                    pub.publish(twist)
-                elif key == Key.esc:
-                    return False
-            except AttributeError:  
-                pass
+    def on_press(key):
         try:
-            listener = Listener(on_press=on_press, on_release=on_release)
-            listener.start()  
-        except KeyboardInterrupt:
+            if key == Key.up:
+                twist.linear.x = 1
+            elif key == Key.down:
+                twist.linear.x = -1
+            elif key == Key.left:
+                twist.angular.z = 1
+            elif key == Key.right:
+                twist.angular.z = -1
+            elif key == Key.esc:
+                print('Bye Bye')
+                rospy.signal_shutdown("Escape key pressed")
+            else:
+                pass
+        except AttributeError:
             pass
+
+    def on_release(key):
+        try:
+            if key == Key.up:
+                twist.linear.x = 0
+            elif key == Key.down:
+                twist.linear.x = 0
+            elif key == Key.left:
+                twist.angular.z = 0
+            elif key == Key.right:
+                twist.angular.z = 0
+        except AttributeError:  
+            pass
+    listener = Listener(on_press=on_press, on_release=on_release)
+    listener.start()  
+    print('Press the arrow keys to move','\n---------------------------','\nPress "esc" key to quit.')
+    while not rospy.is_shutdown():
+        pub.publish(twist)
         rate.sleep()
+        
 if __name__ == '__main__':
     try:
         teleoperation()
     except rospy.ROSInterruptException:
         pass
-    
+
 
 
         
