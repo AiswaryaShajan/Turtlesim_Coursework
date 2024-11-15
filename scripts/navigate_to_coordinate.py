de61a01de6b45f3ff0fa25e9487923f61b815819
Aiswarya Shajan <AS4213@live.mdx.ac.uk>
2024-11-14 22:39:17 -0800

    Fix issue: Callback not having memory of the the angle difference becoming < 0.1.
    There was a brief moment where the velocity is set to zero. But as the callback is continous, every time a pose message comes in, it starts afresh without remembering that the turtle aligned for that instant.
    
    Implement a state flag so that this memory is saved.
    
    Need to check for linear motion condtion.

--- scripts/navigate_to_coordinate.py
+++ scripts/navigate_to_coordinate.py
@@ -8,37 +8,32 @@ rospy.init_node('navigation_node', anonymous=True)
 pub=rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 a, b= map(float, input('Enter the coordinates separated by a space. Make sure the values are between 0 and 11   ').split()) #input () takes the user input as a string. it is then split into 2 strings. the map function applies the float function across the list...giving two float numbers
 print(f"Lets go to: ({a}, {b}") 
+rotation_done= False
 
 def callback(pose):
     rospy.loginfo('the turtle is at %f, %f, %f', pose.x, pose.y, pose.theta)
     twist = Twist()
+    global rotation_done
     rate = rospy.Rate(10)
     y = b-pose.y
     x = a-pose.x 
     angle_to_target = math.atan2(y, x)
     distance_to_target = math.sqrt((a-pose.x)**2+(b-pose.y)**2)
     angle_difference = abs(angle_to_target - pose.theta)
-    rounded_angle_difference = round(angle_difference,2)
-    print(rounded_angle_difference)
-    if rounded_angle_difference > 0.4:
-        twist.angular.z = 2
-        pub.publish(twist)
-    else: 
-        print('hey the reached theta..almost!') #To check if the else block is executed
-        twist.angular.z = 0
-        pub.publish(twist)
-        print(f'angular velocity is {twist.angular.z}') #To check if the angular velocity is set to zero.
-    
-        if distance_to_target > 0.1:
-            twist.linear.x = 0.5
-        else:
-            twist.linear.x = 0
+    print(angle_difference)
+    if not rotation_done:
+        if angle_difference > 0.1:
+            twist.angular.z = 2
             pub.publish(twist)
-    rate.sleep()
+        else: 
+            twist.angular.z = 0
+            pub.publish(twist)
+            rotation_done=True
+            print(f'angular velocity is {twist.angular.z}') #To check if the angular velocity is set to zero.
+            rate.sleep()
+    else:
+        print('rotation done')
     
-    if distance_to_target < 0.1:
-        print('Destination arrived.. Aloha!')
-        rospy.signal_shutdown()
 def subscriber():
     rospy.Subscriber('/turtle1/pose', Pose, callback )
     rospy.spin()
@@ -47,9 +42,3 @@ if __name__ == '__main__':
         subscriber()
     except rospy.ROSInterruptException:
         pass
-
-
-
-        
-
-        
