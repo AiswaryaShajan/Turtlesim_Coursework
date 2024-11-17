b18c6f8458a2731b33772743b7d96662247b7c59
Aiswarya Shajan <AS4213@live.mdx.ac.uk>
2024-11-17 12:32:24 -0800

    Simplify code
    Try to combine logic from the simple code (simply turn the turtle in) and that of complex code (use last pressed key) to turn the turtle based on the key that was last pressed.
    
    **Reset angular velocity each time the turtle gets back in the boundary.(Significant finding)
    **Disable all the other keys at the boundary except for the last pressed key.

--- scripts/wall_avoidance.py
+++ scripts/wall_avoidance.py
@@ -1,75 +1,60 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 from pynput.keyboard import Key, Listener
 twist = Twist() # Declaring the global variables so that they can be accessed in any of the functions.
 pose= Pose()
-last_pressed_key= None
+last_pressed_key = None
 
 def listener_node():
     rospy.init_node('listener_node', anonymous=True)
-    rate=rospy.Rate(30)
+    rate=rospy.Rate(10)
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     def callback(data): # data is a local variable. it can only be used inside the callback function.
         global pose
         pose=data   #it has to be stored somewhere so that it can be accessed outside the callback.
     rospy.Subscriber('/turtle1/pose', Pose, callback)
+    print('Use keys to move:\n"w" for forward\n"s" for reverse\n"a" for rotate left\n"d" for rotate right.')
     def on_press(key):
-        global twist
-        global pose
-        global last_pressed_key
-        try: 
-            if 1 < pose.x < 10 and 1< pose.y < 10:
-                if key.char == 'w':
-                    twist.linear.x = 2
-                    last_pressed_key= 'w'
-                elif key.char == 'a':
-                    twist.linear.x=0
-                    twist.angular.z= 2
-                    last_pressed_key = 'a'
-                elif key.char == 's':
-                    twist.angular.z=0
-                    twist.linear.x = -2
-                    last_pressed_key = 's'
-                elif key.char == 'd':
-                    twist.linear.x =0
-                    twist.angular.z= -2
-                    last_pressed_key = 'd'
-                pub.publish(twist)
-            else:
-                print('Oops! you are off-limits!')
-                if last_pressed_key in ['w','a', 'd']:
+        global twist, pose, last_pressed_key
+        try:
+            if key.char in ['w','a','s','d']:
+                if 1.6 < pose.x < 9.4 and 1.6<pose.y<9.4: #Boundary condition
+                    print('Turtle is within bounds!')
+                    twist.angular.z = 0 # to reset angular velocity, else even if it was in the boundary, it would circle continously.
                     if key.char == 'w':
-                        twist.linear.x=0
+                        twist.linear.x = 2
                         last_pressed_key = 'w'
                     elif key.char == 'a':
-                        last_pressed_key ='a'
                         twist.angular.z= 2
                     elif key.char == 's':
-                        last_pressed_key = 's'
                         twist.linear.x = -2
-                    elif key.char == 'd':
-                        twist.angular.z= -2
-                        last_pressed_key = 'd'
-                elif last_pressed_key in ['s','a','d']:
-                    if key.char == 'w':
-                        twist.linear.x=2
-                        last_pressed_key = 'w'
-                    elif key.char == 'a':
-                        twist.angular.z=2
-                    elif key.char == 's':
-                        twist.linear.x = 0
                         last_pressed_key = 's'
                     elif key.char == 'd':
                         twist.angular.z= -2
-                elif key.char == 'a':
-                    last_pressed_key='a'
-                    twist.angular.z = 2
-                elif key.char == 'd':
-                    last_pressed_key = 'd'
-                    twist.angular.z = -2
-                pub.publish(twist)
+                elif last_pressed_key in ['w','s']:
+                    twist.angular.z = 3
+                    pub.publish(twist)
+                    rospy.Duration(0.5) # precaution to ensure messages dont get overlapped and turtle doesnt skip boundary.
+                    if last_pressed_key == 'w':
+                        print('Oops!..you are off-limits! press "w" to get back in')
+                        if key.char == 'w':
+                            twist.linear.x=2
+                        if key.char == 's':
+                            twist.linear.x =0
+                            twist.angular.z=0
+                    if last_pressed_key == 's':
+                        print('Oops! ..you are off-limits! press "s" to get back in')
+                        if key.char == 's':
+                            twist.linear.x= -2
+                        if key.char == 'w':
+                            twist.linear.x=0
+                            twist.angular.z=0
+                    if key.char in ['a','d']: #Disabling rotation at boundary
+                        twist.linear.x = 0
+                        twist.angular.z =0
+            pub.publish(twist)
         except AttributeError:
             pass
     def on_release(key):
@@ -82,11 +67,9 @@ def listener_node():
                 pub.publish(twist)
         except AttributeError:
             pass    
-    try:
-        listener = Listener(on_press=on_press, on_release=on_release)
-        listener.start()
-    except KeyboardInterrupt:
-        pass
+    listener = Listener(on_press=on_press, on_release=on_release)
+    listener.start()
+        
     
     while not rospy.is_shutdown():
         rate.sleep()
@@ -96,6 +79,3 @@ if __name__== "__main__":
         listener_node()
     except rospy.ROSInterruptException:
         pass
-
-
-
