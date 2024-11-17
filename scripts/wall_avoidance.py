de49e2c85cebeec54c374cfdda0bf552b739a731
Aiswarya Shajan <AS4213@live.mdx.ac.uk>
2024-11-17 01:04:54 -0800

    save before moving onto wall avoidance branch

--- scripts/wall_avoidance.py
+++ scripts/wall_avoidance.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3 #SIMPLE CODE
+#!/usr/bin/env python3 #COMPLEX CODE
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 from pynput.keyboard import Key, Listener
 twist = Twist() # Declaring the global variables so that they can be accessed in any of the functions.
 pose= Pose()
-in_bound= True
+last_pressed_key= None
 
 def listener_node():
     rospy.init_node('listener_node', anonymous=True)
-    rate=rospy.Rate(10)
+    rate=rospy.Rate(30)
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     def callback(data): # data is a local variable. it can only be used inside the callback function.
         global pose
@@ -18,30 +18,58 @@ def listener_node():
     def on_press(key):
         global twist
         global pose
-        global in_bound
-        try:
-            if 1.5 < pose.x < 9.5 and 1.5< pose.y < 9.5:
-
-                print('turtle is within the bounds')
+        global last_pressed_key
+        try: 
+            if 1 < pose.x < 10 and 1< pose.y < 10:
                 if key.char == 'w':
                     twist.linear.x = 2
+                    last_pressed_key= 'w'
                 elif key.char == 'a':
                     twist.linear.x=0
                     twist.angular.z= 2
+                    last_pressed_key = 'a'
                 elif key.char == 's':
                     twist.angular.z=0
                     twist.linear.x = -2
+                    last_pressed_key = 's'
                 elif key.char == 'd':
                     twist.linear.x =0
                     twist.angular.z= -2
+                    last_pressed_key = 'd'
                 pub.publish(twist)
             else:
-                if key.char in ['w','s','a','d']:
-                    print("Oops..you are off-limits! Let's get you back in")
+                print('Oops! you are off-limits!')
+                if last_pressed_key in ['w','a', 'd']:
+                    if key.char == 'w':
+                        twist.linear.x=0
+                        last_pressed_key = 'w'
+                    elif key.char == 'a':
+                        last_pressed_key ='a'
+                        twist.angular.z= 2
+                    elif key.char == 's':
+                        last_pressed_key = 's'
+                        twist.linear.x = -2
+                    elif key.char == 'd':
+                        twist.angular.z= -2
+                        last_pressed_key = 'd'
+                elif last_pressed_key in ['s','a','d']:
+                    if key.char == 'w':
+                        twist.linear.x=2
+                        last_pressed_key = 'w'
+                    elif key.char == 'a':
+                        twist.angular.z=2
+                    elif key.char == 's':
+                        twist.linear.x = 0
+                        last_pressed_key = 's'
+                    elif key.char == 'd':
+                        twist.angular.z= -2
+                elif key.char == 'a':
+                    last_pressed_key='a'
                     twist.angular.z = 2
-                    twist.linear.x = 2
-                    pub.publish(twist)
-                    
+                elif key.char == 'd':
+                    last_pressed_key = 'd'
+                    twist.angular.z = -2
+                pub.publish(twist)
         except AttributeError:
             pass
     def on_release(key):
@@ -58,9 +86,7 @@ def listener_node():
         listener = Listener(on_press=on_press, on_release=on_release)
         listener.start()
     except KeyboardInterrupt:
-        rospy.signal_shutdown()
-        listener.stop()
-        
+        pass
     
     while not rospy.is_shutdown():
         rate.sleep()
@@ -70,3 +96,5 @@ if __name__== "__main__":
         listener_node()
     except rospy.ROSInterruptException:
         pass
+
+
